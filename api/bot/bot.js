0b1207ee95d6e6e1497ca9d888fb9f653af4e207
Amos Senkao <senkao.a@outlook.com>
2023-02-13 12:12:00 +0900

    Refactored the code for better readibility

--- api/bot/bot.js
+++ api/bot/bot.js
@@ -23,7 +23,6 @@ class Bot {
 		try {
 			const profile = await this.lineClient.getProfile(userId);
 			await users.addUser(profile.displayName, userId);
-			console.log('Got profile name:', profile.displayName);
 			return profile.displayName;
 		} catch (error) {
 			console.log(error);
@@ -63,14 +62,12 @@ class Bot {
 			event.replyToken,
 			replyObject
 		);
-		console.log('Reply', reply);
 		return reply;
 	};
 
 	//  Finally, this function calls all the other functions according to the logical flow
 	handleIncomingEvents = async (event) => {
 		if (event.type !== 'message' || event.message.type !== 'text') {
-			console.log('Not message or empty text');
 			return null;
 		}
 		const userPrompt = event.message.text?.toLowerCase();
@@ -78,7 +75,6 @@ class Bot {
 			const userName = await this.getUserInfo(event);
 			const responseText = await this.fetchAiResponse(userName, userPrompt);
 			const output = await this.sendReply(event, userPrompt, responseText);
-			console.log('Output:', output);
 			return output;
 		} catch (error) {
 			return error;
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const express = require('express');
-const ip = require('ip');
 const api = require('./api');
 const PORT = process.env.PORT || 8080;
 
@@ -11,5 +10,5 @@ app.use('/api', api).get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-	console.log(`App is listening to port: ${PORT} on IP: ${ip.address()}`);
+	console.log(`App is listening to port: ${PORT}`);
 });
