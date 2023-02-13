8d32623a3ecd36e4b1f27c68ce779e9f6e9282ac
Amos Senkao <senkao.a@outlook.com>
2023-02-13 09:03:59 +0900

    Did some debugging

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const ip = require('ip');
 const api = require('./api');
 const PORT = process.env.PORT || 8080;
 
@@ -10,5 +11,5 @@ app.use('/api', api).get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-	console.log(`App is listening to port: ${PORT}`);
+	console.log(`App is listening to port: ${PORT} on IP: ${ip.address()}`);
 });
