84fc23ffd02a3f77ea6c21464dd3af04e60d0c57
Amos Senkao <senkao.a@outlook.com>
2023-02-13 09:52:46 +0900

    Refactored the code for better readibility

--- api/bot/bot.js
+++ api/bot/bot.js
@@ -43,7 +43,7 @@ class Bot {
 				prompt(userName, userPrompt, users.getUerConversations())
 			);
 			console.log(response.data.usage);
-			response.data.choices[0].text;
+			return response.data.choices[0].text;
 		} catch (error) {
 			console.log(error);
 			return error;
--- api/bot/database/db.js
+++ api/bot/database/db.js
@@ -3,13 +3,13 @@ const { MongoClient } = require('mongodb');
 async function database() {
 	const client = new MongoClient(process.env.MONGO_DB_URI);
 	try {
-		const database = await client.db('chat-bot');
-		const users = await database.collection('users');
-		return users;
+		const database = client.db('chat-bot');
+		var users = database.collection('users');
 	} catch (error) {
 		console.log(error);
 	} finally {
-		// client.close();
+		client.close();
+		return users;
 	}
 }
 
